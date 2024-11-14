1cd5e7c7795b39af91e276146bcd6df72877124a
Bijan Varjavand <bvarjavand@gmail.com>
2024-11-14 14:02:55 -0800

    Update wandb_simple.py

--- dags/wandb_simple.py
+++ dags/wandb_simple.py
@@ -1,7 +1,14 @@
-from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 # airflow import
 
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models.dag import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+
+
+# python script to run
 import numpy as np
 
 import ray
@@ -93,13 +100,89 @@ def tune_trainable():
 
     return results.get_best_result().config
 
-# build and run dag
-dag = DAG('wandb_simple', default_args=default_args)
+# run the dag
+with DAG(
+    "wandb_simple",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'on_skipped_callback': another_function, #or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+    description="A tutorial DAG using ray and wandb",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
 
-PythonOperator(dag=dag,
-               task_id='wandb_simple',
-               provide_context=False,
-               python_callable=tune_with_callback,
-               #op_args=['arguments_passed_to_callable'],
-               #op_kwargs={'keyword_argument':'which will be passed to function'},
-              )
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id="run_script",
+        bash_command="wandb login",
+    )
+
+    t2 = BashOperator(
+        task_id="sleep",
+        depends_on_past=False,
+        bash_command="sleep 2",
+        retries=3,
+    )
+    t1.doc_md = textwrap.dedent(
+        """\
+    #### Task Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](https://imgs.xkcd.com/comics/fixing_problems.png)
+    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
+    """
+    )
+
+    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG; OR
+    dag.doc_md = """
+    This is a documentation placed anywhere
+    """  # otherwise, type it like this
+
+    t3 = PythonOperator(
+        task_id="templated",
+        depends_on_past=True,
+        provide_context=False,
+        python_callable=tune_with_callback,
+    )
+    t3.doc_md = textwrap.dedent(
+        """\
+    #### Task Documentation
+    Runs the ray+wandb example
+    **Credit:** https://docs.ray.io/en/latest/tune/examples/tune-wandb.html
+    """
+
+    [t1, t2] >> t3
+
+# # build and run dag
+# dag = DAG('wandb_simple', default_args=default_args)
+
+# PythonOperator(dag=dag,
+#                task_id='wandb_simple',
+#                provide_context=False,
+#                python_callable=tune_with_callback,
+#                #op_args=['arguments_passed_to_callable'],
+#                #op_kwargs={'keyword_argument':'which will be passed to function'},
+#               )
