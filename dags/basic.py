1d35f4376f278aa8c1df63a154e4185e40b86348
Bijan Varjavand <bvarjavand@gmail.com>
2024-11-20 10:31:26 -0800

    Create basic.py

---
+++ dags/basic.py
@@ -0,0 +1,34 @@
+from airflow.models.dag import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime, timedelta
+import textwrap
+
+#### Defining the python code
+
+def basic():
+    myvar = "Hello World!"
+    print(myvar)
+
+
+with DAG(
+    "wandb_simple",
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 0, # or 1
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A tutorial DAG using ray and wandb",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    dag.doc_md = """
+    This is documentation placed anywhere
+    """
+
+    PythonOperator(task_id="basic", python_callable=basic)
